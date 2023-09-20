9d82e2c147129e18c234b9e1c1519bd5c5529c7e
launchd <me@zpld.me>
2023-09-19 19:10:48 -0500

    rework dark mode handling for efficiency

--- static/script.js
+++ static/script.js
@@ -1,20 +1,4 @@
-function toggleTheme() {
-    const toggled = document.body.classList.toggle('dark-mode');
-    localStorage.setItem('dark-mode', toggled);
-    document.getElementById('theme-toggle-input').checked = toggled;
-}
-
-document.addEventListener('DOMContentLoaded', (event) => {
-    const prefersDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-    const darkMode = localStorage.getItem('dark-mode') === 'true' || prefersDarkMode;
-    document.body.classList.toggle('dark-mode', darkMode);
-    document.getElementById('theme-toggle-input').checked = darkMode;
-
-    const textArea = document.querySelector('textarea');
-    if (textArea) {
-        textArea.addEventListener('input', () => {
-            textArea.style.height = 'auto';
-            textArea.style.height = textArea.scrollHeight + 'px';
-        });
-    }
-});
+/*
+ * this script might be used for something later but right now it simply exists as a placeholder 
+ * ~ launchd
+*/
